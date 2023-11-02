92091dace830b9168efe24a8154d103ed6f887af
Unai5 <unaiv2004@gmail.com>
2023-11-02 10:17:23 +0100

    Formulario libros en proceso (es el más complejo) y modificaciones en Consultas.

---
+++ CapaPresentacion/frmLibros.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace CapaPresentacion
+{
+    partial class frmLibros
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnVolver = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnPrestamos = new System.Windows.Forms.Button();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.btnNuevoLibro = new System.Windows.Forms.Button();
+            this.txtBuscar = new System.Windows.Forms.TextBox();
+            this.dgvLibros = new System.Windows.Forms.DataGridView();
+            this.btnCategorias = new System.Windows.Forms.Button();
+            this.btnAutores = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLibros)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnVolver
+            // 
+            this.btnVolver.Location = new System.Drawing.Point(22, 573);
+            this.btnVolver.Name = "btnVolver";
+            this.btnVolver.Size = new System.Drawing.Size(120, 44);
+            this.btnVolver.TabIndex = 1;
+            this.btnVolver.Text = "Volver";
+            this.btnVolver.UseVisualStyleBackColor = true;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(849, 492);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(120, 44);
+            this.btnEliminar.TabIndex = 2;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            // 
+            // btnPrestamos
+            // 
+            this.btnPrestamos.Location = new System.Drawing.Point(671, 492);
+            this.btnPrestamos.Name = "btnPrestamos";
+            this.btnPrestamos.Size = new System.Drawing.Size(120, 44);
+            this.btnPrestamos.TabIndex = 3;
+            this.btnPrestamos.Text = "Préstamos";
+            this.btnPrestamos.UseVisualStyleBackColor = true;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(882, 12);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(120, 44);
+            this.btnBuscar.TabIndex = 4;
+            this.btnBuscar.Text = "Buscar Libro";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // btnNuevoLibro
+            // 
+            this.btnNuevoLibro.Location = new System.Drawing.Point(199, 492);
+            this.btnNuevoLibro.Name = "btnNuevoLibro";
+            this.btnNuevoLibro.Size = new System.Drawing.Size(120, 44);
+            this.btnNuevoLibro.TabIndex = 5;
+            this.btnNuevoLibro.Text = "Nuevo Libro";
+            this.btnNuevoLibro.UseVisualStyleBackColor = true;
+            // 
+            // txtBuscar
+            // 
+            this.txtBuscar.Location = new System.Drawing.Point(624, 12);
+            this.txtBuscar.Name = "txtBuscar";
+            this.txtBuscar.Size = new System.Drawing.Size(241, 22);
+            this.txtBuscar.TabIndex = 6;
+            this.txtBuscar.Text = "Título...";
+            // 
+            // dgvLibros
+            // 
+            this.dgvLibros.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvLibros.Location = new System.Drawing.Point(12, 69);
+            this.dgvLibros.Name = "dgvLibros";
+            this.dgvLibros.RowHeadersWidth = 51;
+            this.dgvLibros.RowTemplate.Height = 24;
+            this.dgvLibros.Size = new System.Drawing.Size(1078, 401);
+            this.dgvLibros.TabIndex = 7;
+            // 
+            // btnCategorias
+            // 
+            this.btnCategorias.Location = new System.Drawing.Point(525, 492);
+            this.btnCategorias.Name = "btnCategorias";
+            this.btnCategorias.Size = new System.Drawing.Size(120, 44);
+            this.btnCategorias.TabIndex = 8;
+            this.btnCategorias.Text = "Categorías";
+            this.btnCategorias.UseVisualStyleBackColor = true;
+            // 
+            // btnAutores
+            // 
+            this.btnAutores.Location = new System.Drawing.Point(388, 492);
+            this.btnAutores.Name = "btnAutores";
+            this.btnAutores.Size = new System.Drawing.Size(120, 44);
+            this.btnAutores.TabIndex = 9;
+            this.btnAutores.Text = "Autores";
+            this.btnAutores.UseVisualStyleBackColor = true;
+            // 
+            // frmLibros
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(1105, 629);
+            this.Controls.Add(this.btnAutores);
+            this.Controls.Add(this.btnCategorias);
+            this.Controls.Add(this.dgvLibros);
+            this.Controls.Add(this.txtBuscar);
+            this.Controls.Add(this.btnNuevoLibro);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.btnPrestamos);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnVolver);
+            this.Name = "frmLibros";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.frmLibros_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLibros)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnVolver;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnPrestamos;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Button btnNuevoLibro;
+        private System.Windows.Forms.TextBox txtBuscar;
+        private System.Windows.Forms.DataGridView dgvLibros;
+        private System.Windows.Forms.Button btnCategorias;
+        private System.Windows.Forms.Button btnAutores;
+    }
+}
---
+++ CapaPresentacion/frmLibros.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using CapaDatos;
+using Entidades;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CapaPresentacion
+{
+    public partial class frmLibros : Form
+    {
+        public frmLibros()
+        {
+            
+        }
+
+        private void frmLibros_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+
+            dgvLibros.Rows.Clear();
+
+            string titulo = txtBuscar.Text;
+
+            string errores;
+
+            List<Libro> libros = Consultas.BuscarLibroPorPorcionTitulo(titulo, out errores);
+            
+            dgvLibros.DataSource = libros;
+
+        }
+    }
+}
