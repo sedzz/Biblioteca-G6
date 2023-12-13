af525c8c99be24ca5c301d9bb153a040e93e89b3
Unai5 <unaiv2004@gmail.com>
2023-12-13 21:30:48 +0100

    Añadidos comentarios y summarys en los métodos de GestorBiblioteca y de Consultas. Añadida una nueva consulta, ObtenerTodosLosLectores. Formulario FrmLectores Diseñado.

---
+++ CapaPresentacion/FrmLectores.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace CapaPresentacion
+{
+    partial class FrmLectores
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.dgvSocios = new System.Windows.Forms.DataGridView();
+            this.btnMostrarTodos = new System.Windows.Forms.Button();
+            this.btnBuscarNombre = new System.Windows.Forms.Button();
+            this.btnBuscarId = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btnNuevoSocio = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSocios)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Sans Serif Collection", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(300, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(454, 49);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "SOCIOS DE LA BIBLIOTECA";
+            // 
+            // dgvSocios
+            // 
+            this.dgvSocios.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvSocios.Location = new System.Drawing.Point(44, 61);
+            this.dgvSocios.Name = "dgvSocios";
+            this.dgvSocios.RowHeadersWidth = 51;
+            this.dgvSocios.RowTemplate.Height = 24;
+            this.dgvSocios.Size = new System.Drawing.Size(978, 289);
+            this.dgvSocios.TabIndex = 1;
+            // 
+            // btnMostrarTodos
+            // 
+            this.btnMostrarTodos.Location = new System.Drawing.Point(96, 385);
+            this.btnMostrarTodos.Name = "btnMostrarTodos";
+            this.btnMostrarTodos.Size = new System.Drawing.Size(133, 63);
+            this.btnMostrarTodos.TabIndex = 2;
+            this.btnMostrarTodos.Text = "Mostrar tod@s";
+            this.btnMostrarTodos.UseVisualStyleBackColor = true;
+            // 
+            // btnBuscarNombre
+            // 
+            this.btnBuscarNombre.Location = new System.Drawing.Point(309, 385);
+            this.btnBuscarNombre.Name = "btnBuscarNombre";
+            this.btnBuscarNombre.Size = new System.Drawing.Size(144, 63);
+            this.btnBuscarNombre.TabIndex = 3;
+            this.btnBuscarNombre.Text = "Buscar por trozo de nombre";
+            this.btnBuscarNombre.UseVisualStyleBackColor = true;
+            // 
+            // btnBuscarId
+            // 
+            this.btnBuscarId.Location = new System.Drawing.Point(722, 385);
+            this.btnBuscarId.Name = "btnBuscarId";
+            this.btnBuscarId.Size = new System.Drawing.Size(133, 63);
+            this.btnBuscarId.TabIndex = 4;
+            this.btnBuscarId.Text = "Buscar por número de carnet";
+            this.btnBuscarId.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(459, 405);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(180, 22);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(861, 405);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(168, 22);
+            this.textBox2.TabIndex = 6;
+            // 
+            // btnNuevoSocio
+            // 
+            this.btnNuevoSocio.Location = new System.Drawing.Point(227, 514);
+            this.btnNuevoSocio.Name = "btnNuevoSocio";
+            this.btnNuevoSocio.Size = new System.Drawing.Size(146, 65);
+            this.btnNuevoSocio.TabIndex = 7;
+            this.btnNuevoSocio.Text = "NUEVO SOCIO";
+            this.btnNuevoSocio.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(626, 514);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(129, 65);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "VOLVER";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // FrmLectores
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1082, 653);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnNuevoSocio);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnBuscarId);
+            this.Controls.Add(this.btnBuscarNombre);
+            this.Controls.Add(this.btnMostrarTodos);
+            this.Controls.Add(this.dgvSocios);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmLectores";
+            this.Text = "FrmLectores";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSocios)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dgvSocios;
+        private System.Windows.Forms.Button btnMostrarTodos;
+        private System.Windows.Forms.Button btnBuscarNombre;
+        private System.Windows.Forms.Button btnBuscarId;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btnNuevoSocio;
+        private System.Windows.Forms.Button button1;
+    }
+}
